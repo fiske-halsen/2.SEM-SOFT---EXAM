9cf1a16f805b6a86407c476c72ea52be51164ece
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2022-11-22 10:47:52 +0100

    added dummy test project to microservice1

---
+++ Microservice1.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Microservice1.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
