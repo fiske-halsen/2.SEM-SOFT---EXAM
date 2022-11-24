c6a14e3d2f8a2ab451461c766e3d971516254747
LAPTOP-69AO34FQ\lukas <lukasbangstoltz@gmail.com>
2022-11-24 20:31:40 +0100

    please let it work now

--- Application/EmailService/WeatherForecast.cs
+++ Application/EmailService/WeatherForecast.cs
@@ -7,7 +7,7 @@ namespace EmailService
         public int TemperatureC { get; set; }
         //cheeky little comment!!!
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        //another comment
         public string? Summary { get; set; }
     }
 }
