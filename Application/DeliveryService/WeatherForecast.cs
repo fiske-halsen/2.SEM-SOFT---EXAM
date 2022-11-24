b8bc5ff24f6a581f8bd9212f0abc03aebf7f309f
LAPTOP-69AO34FQ\lukas <lukasbangstoltz@gmail.com>
2022-11-24 21:34:04 +0100

    trigger for only delivery service

--- Application/DeliveryService/WeatherForecast.cs
+++ Application/DeliveryService/WeatherForecast.cs
@@ -5,7 +5,7 @@ namespace DeliveryService
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
-
+        //Teest comment
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
