e306a8113d1f6625b4c4ad75fc38e26468edb2f4
Sumit Dey <sumitdey0007@gmail.com>
2022-11-25 10:25:06 +0100

    models til feedback service lavet

---
+++ Application/FeedbackService/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace FeedbackService.Models
+{
+    public class Review
+    {
+        public string name { get; set; }
+        public string restaurant{ get; set; }
+        public string reviewText{ get; set; }
+        public DateTime reviewDate{ get; set; }
+        public int rating{ get; set; }
+
+    }
+}
