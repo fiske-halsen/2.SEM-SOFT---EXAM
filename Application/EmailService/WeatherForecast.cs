eaffb8fea83cf86e7bea014035b5133edd56afee
Phillip <phillip_andersen@hotmail.com>
2022-11-25 11:46:50 +0100

    test

--- Application/EmailService/WeatherForecast.cs
+++ Application/EmailService/WeatherForecast.cs
@@ -3,7 +3,7 @@ namespace EmailService
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
+        // TEST WOW
         public int TemperatureC { get; set; }
         //cheeky little comment!!!
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
