a2e65237ae14f6e9d65bd64b23c9121a3ba71dba
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2022-11-30 18:04:37 +0100

    common added

---
+++ Application/Common/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public List<MenuItem> Items { get; set; } = new List<MenuItem>();
+        public float Total { get; set; }
+        public string Restaurant { get; set; }
+        public string Customer { get; set; }
+    }
+}
