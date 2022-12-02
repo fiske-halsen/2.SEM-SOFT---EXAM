52c8412a98a8068d2530de08460f95403d07eb4a
Sumit Dey <sumitdey0007@gmail.com>
2022-12-02 14:25:47 +0100

    reviewmodels and dto done

--- Application/FeedbackService/Models/Review.cs
+++ Application/FeedbackService/Models/Review.cs
@@ -3,6 +3,9 @@
     public class Review
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RestaurantId{ get; set; }
+        public int DeliveryDriverId { get; set; }
         public string ReviewText{ get; set; }
         public DateTime ReviewDate{ get; set; } = DateTime.UtcNow;
         public int OrderId { get; set; }
