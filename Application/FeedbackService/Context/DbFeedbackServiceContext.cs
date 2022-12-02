a3c418b84ddc96832305f40eb261d1e1f0b262b5
Sumit Dey <sumitdey0007@gmail.com>
2022-12-02 12:09:17 +0100

    data storage done

--- Application/FeedbackService/Context/DbFeedbackServiceContext.cs
+++ Application/FeedbackService/Context/DbFeedbackServiceContext.cs
@@ -6,12 +6,11 @@ using System.Reflection.Emit;
 namespace FeedbackService.Context
 {
 
-        public class DbOrderServiceContext : DbContext
+        public class DBFeedbackServiceContext : DbContext
         {
-            public DbOrderServiceContext(DbContextOptions<DbOrderServiceContext> options) : base(options) { }
+            public DBFeedbackServiceContext(DbContextOptions<DBFeedbackServiceContext> options) : base(options) { }
 
             public DbSet<Review> Reviews { get; set; }
-            public DbSet<Complaint> Complaints { get; set; }
 
             protected override void OnModelCreating(ModelBuilder builder)
             {
@@ -21,14 +20,10 @@ namespace FeedbackService.Context
         private void Seed(ModelBuilder builder)
         {
             builder.Entity<Review>().HasData(
-              new Review { Id = 1, UserId = 1, RestaurantId = 2, DeliveryDriverId = 3, ReviewText = "Maden var god og blev leveret hurtigt", ReviewDate = DateTime.Now, Rating = 5 },
-              new Review { Id = 2, UserId = 1, RestaurantId = 2, DeliveryDriverId = 3, ReviewText = "Maden var dårlig og blev leveret efter 3 timer", ReviewDate = DateTime.Now, Rating = 1 }
+              new Review { Id = 1, UserId = 1, RestaurantId = 2, DeliveryDriverId = 3, ReviewText = "Maden var god og blev leveret hurtigt", ReviewDate = DateTime.Now, OrderId = 2, Rating = 5 },
+              new Review { Id = 2, UserId = 1, RestaurantId = 2, DeliveryDriverId = 3, ReviewText = "Maden var dårlig og blev leveret efter 3 timer", ReviewDate = DateTime.Now, OrderId = 1, Rating = 1 }
 
               );
-            builder.Entity<Complaint>().HasData(
-                new Complaint { Id = 1, UserId = 1, RestaurantId = 2, DeliveryDriverID = 3, ComplaintText = "Maden var kold, jeg vil gerne have mine penge tilbage!", ComplaintDate = DateTime.Today, OrderId = 1 },
-                new Complaint { Id = 2, UserId = 1, RestaurantId = 2, DeliveryDriverID = 3, ComplaintText = "Maden var kold, jeg vil gerne have mine penge tilbage!", ComplaintDate = DateTime.Today, OrderId = 1 });
-
         }
     }
     }
--- Application/FeedbackService/DTO/CreateReviewDto.cs
+++ Application/FeedbackService/DTO/CreateReviewDto.cs
@@ -2,12 +2,10 @@
 {
     public class CreateReviewDto
     {
-        public int Id { get; set; }
-        public int UserID { get; set; }
-        public int RestaurantId { get; set; }
-        public int DeliveryDriverId { get; set; }
         public string ReviewText { get; set; }
         public DateTime ReviewDate { get; set; }
+        public int OrderId { get; set; }
         public int Rating { get; set; }
+
     }
 }
