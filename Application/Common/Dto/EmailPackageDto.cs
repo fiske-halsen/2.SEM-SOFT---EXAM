41948a05b047740e16f37d91a2269fc47752dd56
Phillip <phillip_andersen@hotmail.com>
2022-12-07 14:30:48 +0100

    paramaterized email topic

--- Application/Common/Dto/EmailPackageDto.cs
+++ Application/Common/Dto/EmailPackageDto.cs
@@ -3,6 +3,7 @@
     public class EmailPackageDto
     {
         public string Email { get; set; }
+        public string Subject { get; set; }
         public string Message { get; set; }
     }
 }
