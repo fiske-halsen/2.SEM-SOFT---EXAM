897234f723f3c166cf6fd2c88a39aa788e1b00db
Phillip <phillip_andersen@hotmail.com>
2022-12-09 17:53:51 +0100

    identity server not configurated to secure deliveryService

--- Application/DeliveryService/Controllers/DeliveryController.cs
+++ Application/DeliveryService/Controllers/DeliveryController.cs
@@ -1,6 +1,7 @@
 ﻿using DeliveryService.DTO;
 using DeliveryService.Models;
 using DeliveryService.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeliveryService.Controllers
@@ -16,29 +17,38 @@ namespace DeliveryService.Controllers
             _deliverySerivce = deliverySerivce;
         }
 
+        [Authorize]
         [HttpPost]
-        public async Task<bool> CreateDelivery([FromBody]CreateDeliveryDTO createDeliveryDTO)
+        public async Task<bool>
+            CreateDelivery(
+                [FromBody]
+                CreateDeliveryDTO createDeliveryDTO) // This method should not be http based, we are making this event based for real time deliveries
         {
             return await _deliverySerivce.CreateDelivery(createDeliveryDTO);
         }
 
+        [Authorize]
         [HttpGet("delivery-persons/{deliveryPersonId}")]
         public async Task<List<Delivery>> GetDeliveriesByDeliveryPersonId(int deliveryPersonId)
         {
             return await _deliverySerivce.GetDeliveriesByDeliveryPersonId(deliveryPersonId);
         }
 
+        [Authorize]
         [HttpGet("orders/{orderId}")]
         public async Task<Delivery> GetDeliveryByOrderId(int orderId)
         {
             return await _deliverySerivce.GetDeliveryByOrderId(orderId);
         }
+
+        [Authorize]
         [HttpGet("customers/{userEmail}")]
         public async Task<List<Delivery>> GetDeliveriesByUserEmail(string userEmail)
         {
             return await _deliverySerivce.GetDeliveriesByUserEmail(userEmail);
         }
 
+        [Authorize]
         [HttpPatch("orders/cancel/{orderId}")]
         public async Task<bool> UpdateDeliveryToIsCancelled(int orderId)
         {
