0ba42bec200014b4418f915ef8b75a942cfca932
Phillip <phillip_andersen@hotmail.com>
2022-12-09 17:37:30 +0100

    added new features to deliveryservices, such as updating a delivery to delivered

--- Application/DeliveryService/Repository/DeliveryRepository.cs
+++ Application/DeliveryService/Repository/DeliveryRepository.cs
@@ -1,4 +1,5 @@
-﻿using DeliveryService.Context;
+﻿using System.Diagnostics;
+using DeliveryService.Context;
 using DeliveryService.DTO;
 using DeliveryService.Models;
 using Microsoft.EntityFrameworkCore;
@@ -13,7 +14,11 @@ namespace DeliveryService.Repository
         public Task<List<Delivery>> GetDeliveriesByUserEmail(string UserEmail);
         public Task<Delivery> GetDeliveryPersonWhereIsDeliveredFalse(int deliverPersonId);
         public Task<bool> UpdateDeliveryToIsCancelled(Delivery delivery);
+        public Task<Delivery> GetDeliveryByDeliveryId(int deliveryId);
+        public Task<bool> UpdateDeliveryAsDelivered(Delivery delivery);
+
     }
+
     public class DeliveryRepository : IDeliveryRepository
     {
         private readonly DbApplicationContext _applicationContext;
@@ -23,39 +28,110 @@ namespace DeliveryService.Repository
             _applicationContext = applicationContext;
         }
 
+        /// <summary>
+        /// Creates a new delivery and saves it in the database
+        /// </summary>
+        /// <param name="delivery"></param>
+        /// <returns></returns>
         public async Task<bool> CreateDelivery(Delivery delivery)
         {
-                
-                _applicationContext.Deliveries.Add(delivery);
-                await _applicationContext.SaveChangesAsync();
-                return true;
+            _applicationContext.Deliveries.Add(delivery);
+            await _applicationContext.SaveChangesAsync();
+            return true;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="deliveryPersonId"></param>
+        /// <returns></returns>
         public async Task<List<Delivery>> GetDeliveriesByDeliveryPersonId(int deliveryPersonId)
         {
-            return await _applicationContext.Deliveries.Where(x => x.DeliveryPersonId == deliveryPersonId).ToListAsync();
+            return await _applicationContext.Deliveries.Where(x => x.DeliveryPersonId == deliveryPersonId)
+                .ToListAsync();
         }
 
+        /// <summary>
+        /// Gets all deliveries by user email from the database
+        /// </summary>
+        /// <param name="userEmail"></param>
+        /// <returns></returns>
         public async Task<List<Delivery>> GetDeliveriesByUserEmail(string userEmail)
         {
             return await _applicationContext.Deliveries.Where(x => x.UserEmail == userEmail).ToListAsync();
         }
 
+        /// <summary>
+        /// Gets all deliveries by a deliveryPersonId that arent delivered
+        /// </summary>
+        /// <param name="deliveryPersonId"></param>
+        /// <returns></returns>
         public async Task<Delivery> GetDeliveryPersonWhereIsDeliveredFalse(int deliveryPersonId)
         {
-            return await _applicationContext.Deliveries.Where(x => x.DeliveryPersonId == deliveryPersonId && !x.IsDelivered).FirstOrDefaultAsync();
+            return await _applicationContext.Deliveries
+                .Where(x => x.DeliveryPersonId == deliveryPersonId && !x.IsDelivered).FirstOrDefaultAsync();
         }
 
+        /// <summary>
+        /// Gets a delivery by order id
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
         public async Task<Delivery> GetDeliveryByOrderId(int orderId)
         {
             return await _applicationContext.Deliveries.Where(x => x.OrderId == orderId).FirstOrDefaultAsync();
         }
 
+        /// <summary>
+        /// Updates delivery to is cancelled in the database
+        /// </summary>
+        /// <param name="delivery"></param>
+        /// <returns></returns>
         public async Task<bool> UpdateDeliveryToIsCancelled(Delivery delivery)
         {
-            _applicationContext.Deliveries.Update(delivery);
-            await _applicationContext.SaveChangesAsync();
-            return true;
+            try
+            {
+                _applicationContext.Deliveries.Update(delivery);
+                await _applicationContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets a delivery by delivery id from the database
+        /// </summary>
+        /// <param name="deliveryId"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<Delivery> GetDeliveryByDeliveryId(int deliveryId)
+        {
+            return _applicationContext.Deliveries.Where(_ => _.DeliveryId == deliveryId).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Updates the status of a delivery to delivered
+        /// </summary>
+        /// <param name="deliveryId"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<bool> UpdateDeliveryAsDelivered(Delivery delivery)
+        {
+            try
+            {
+                _applicationContext.Deliveries.Update(delivery);
+                await _applicationContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
         }
     }
 }
--- Application/DeliveryService/Services/DeliveryService.cs
+++ Application/DeliveryService/Services/DeliveryService.cs
@@ -6,13 +6,16 @@ using Common.ErrorModels;
 namespace DeliveryService.Services
 {
     public interface IDeliverySerivce
-    { 
+    {
         public Task<bool> CreateDelivery(CreateDeliveryDTO createDeliveryDTO);
         public Task<List<Delivery>> GetDeliveriesByDeliveryPersonId(int DeliveryPersonId);
-        public Task<Delivery> GetDeliveryByOrderId(int OrderId);
-        public Task<List<Delivery>> GetDeliveriesByUserEmail(string UserEmail);
-        public Task<bool> UpdateDeliveryToIsCancelled(int OrderId);
+        public Task<Delivery> GetDeliveryByOrderId(int orderId);
+        public Task<List<Delivery>> GetDeliveriesByUserEmail(string userEmail);
+        public Task<bool> UpdateDeliveryToIsCancelled(int orderId);
+        public Task<Delivery> GetDeliveryByDeliveryId(int deliveryId);
+        public Task<bool> UpdateDeliveryAsDelivered(int deliveryId);
     }
+
     public class DeliveryService : IDeliverySerivce
     {
         private readonly IDeliveryRepository _deliveryRepository;
@@ -22,61 +25,162 @@ namespace DeliveryService.Services
             _deliveryRepository = deliveryRepository;
         }
 
-        public async Task <bool> CheckIfHasDelivery(int deliveryPersonId)
+        /// <summary>
+        /// Checks if a person has a delivery...
+        /// </summary>
+        /// <param name="deliveryPersonId"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpStatusException"></exception>
+        public async Task<bool> CheckIfHasDelivery(int deliveryPersonId)
         {
             var delivery = await _deliveryRepository.GetDeliveryPersonWhereIsDeliveredFalse(deliveryPersonId);
-            if(delivery != null)
+            if (delivery != null)
             {
-                throw new HttpStatusException(StatusCodes.Status400BadRequest, "U cannot have more than 1 delivery at a time");
+                throw new HttpStatusException(StatusCodes.Status400BadRequest,
+                    "U cannot have more than 1 delivery at a time");
             }
+
             return false;
         }
 
+        /// <summary>
+        /// Creates a new delivery
+        /// </summary>
+        /// <param name="createDeliveryDTO"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpStatusException"></exception>
         public async Task<bool> CreateDelivery(CreateDeliveryDTO createDeliveryDTO)
         {
             if (!createDeliveryDTO.UserEmail.Contains("@"))
             {
-                throw new HttpStatusException(StatusCodes.Status400BadRequest, "Email must containt character '@' to be valid");
+                throw new HttpStatusException(StatusCodes.Status400BadRequest,
+                    "Email must containt character '@' to be valid");
             }
+
+            // Check if guy already has a delivery that is not delivered
+            var deliveries = await _deliveryRepository.GetDeliveriesByUserEmail(createDeliveryDTO.UserEmail);
+
+            // If theres any where they ahve a delivery that isnt delivered
+            if (deliveries.Any(_ => _.IsDelivered == false))
+            {
+                throw new HttpStatusException(StatusCodes.Status400BadRequest,
+                    $"Delivery guy with email {createDeliveryDTO.UserEmail} already has a active delivery");
+            }
+
             var delivery = new Delivery
             {
                 DeliveryPersonId = createDeliveryDTO.DeliveryPersonId,
                 OrderId = createDeliveryDTO.OrderId,
                 UserEmail = createDeliveryDTO.UserEmail,
                 TimeToDelivery = createDeliveryDTO.TimeToDelivery
-            }; 
+            };
 
             await _deliveryRepository.CreateDelivery(delivery);
             return true;
         }
 
+        /// <summary>
+        /// Gets delivery by a delivery person id
+        /// </summary>
+        /// <param name="deliveryPersonId"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpStatusException"></exception>
         public async Task<List<Delivery>> GetDeliveriesByDeliveryPersonId(int deliveryPersonId)
         {
             var deliveries = await _deliveryRepository.GetDeliveriesByDeliveryPersonId(deliveryPersonId);
-            if (!deliveries.Any()) { throw new HttpStatusException(StatusCodes.Status400BadRequest, "U do not have any deliveries"); }
+            if (!deliveries.Any())
+            {
+                throw new HttpStatusException(StatusCodes.Status400BadRequest, "U do not have any deliveries");
+            }
+
             return deliveries;
         }
 
+        /// <summary>
+        /// Gets delivery by a user email
+        /// </summary>
+        /// <param name="userEmail"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpStatusException"></exception>
         public async Task<List<Delivery>> GetDeliveriesByUserEmail(string userEmail)
         {
             var deliveries = await _deliveryRepository.GetDeliveriesByUserEmail(userEmail);
-            if (!deliveries.Any()) { throw new HttpStatusException(StatusCodes.Status400BadRequest, "U do not have any deliveries"); }
+            if (!deliveries.Any())
+            {
+                throw new HttpStatusException(StatusCodes.Status400BadRequest, "U do not have any deliveries");
+            }
+
             return deliveries;
         }
 
-        public async Task<Delivery> GetDeliveryByOrderId(int orderId)
+        /// <summary>
+        /// Gets deliveries by a given delivery Id
+        /// </summary>
+        /// <param name="deliveryId"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpStatusException"></exception>
+        public async Task<Delivery> GetDeliveryByDeliveryId(int deliveryId)
         {
-            var delivery = await _deliveryRepository.GetDeliveryByOrderId(orderId);
-            if(delivery == null) { throw new HttpStatusException(StatusCodes.Status400BadRequest, $"Order with given id = {orderId} does not exist"); }
+            var delivery = await _deliveryRepository.GetDeliveryByDeliveryId(deliveryId);
+
+            if (delivery == null)
+            {
+                throw new HttpStatusException(StatusCodes.Status400BadRequest, "Delivery does not exist");
+            }
+
             return delivery;
         }
 
-        public async Task<bool> UpdateDeliveryToIsCancelled(int OrderId)
+        /// <summary>
+        /// Gets a delivery by a order id
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpStatusException"></exception>
+        public async Task<Delivery> GetDeliveryByOrderId(int orderId)
         {
-            var delivery = await _deliveryRepository.GetDeliveryByOrderId(OrderId);
-            if (delivery == null) { 
-                throw new HttpStatusException(StatusCodes.Status400BadRequest, "Delivery with given id does not exists "); 
+            var delivery = await _deliveryRepository.GetDeliveryByOrderId(orderId);
+            if (delivery == null)
+            {
+                throw new HttpStatusException(StatusCodes.Status400BadRequest,
+                    $"Order with given id = {orderId} does not exist");
             }
+
+            return delivery;
+        }
+
+        /// <summary>
+        /// Updates a given delivery to is delivered
+        /// </summary>
+        /// <param name="deliveryId"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateDeliveryAsDelivered(int deliveryId)
+        {
+            var delivery = await _deliveryRepository.GetDeliveryByDeliveryId(deliveryId);
+
+            if (delivery == null)
+            {
+                throw new HttpStatusException(StatusCodes.Status400BadRequest, "Delivery does not exist");
+            }
+
+            return await _deliveryRepository.UpdateDeliveryAsDelivered(delivery);
+        }
+
+        /// <summary>
+        /// Updates a delivery to is cancelled
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        /// <exception cref="HttpStatusException"></exception>
+        public async Task<bool> UpdateDeliveryToIsCancelled(int orderId)
+        {
+            var delivery = await _deliveryRepository.GetDeliveryByOrderId(orderId);
+            if (delivery == null)
+            {
+                throw new HttpStatusException(StatusCodes.Status400BadRequest,
+                    "Delivery with given id does not exists ");
+            }
+
             delivery.isCancelled = true;
             delivery.TimeToDelivery = DateTime.MinValue;
             await _deliveryRepository.UpdateDeliveryToIsCancelled(delivery);
