d4db50b41930d1f1700979a41983e3ce5bae4d05
Sumit Dey <sumitdey0007@gmail.com>
2022-12-09 16:30:39 +0100

    queries and mutations added for reviewservice

--- Application/GraphqlDemo/Operations/Query.cs
+++ Application/GraphqlDemo/Operations/Query.cs
@@ -1,5 +1,6 @@
 ﻿using Bogus;
 using Common.Dto;
+using FeedbackService.DTO;
 using GraphqlDemo.Services;
 
 namespace GraphqlDemo.Operations
@@ -7,10 +8,12 @@ namespace GraphqlDemo.Operations
     public class Query
     {
         private readonly IOrderServiceCommunicator _orderServiceCommunicator;
+        private readonly IReviewServiceCommunicator _reviewServiceCommunicator;
 
-        public Query(IOrderServiceCommunicator orderServiceCommunicator)
+        public Query(IOrderServiceCommunicator orderServiceCommunicator, IReviewServiceCommunicator reviewServiceCommunicator)
         {
             _orderServiceCommunicator = orderServiceCommunicator;
+            _reviewServiceCommunicator = reviewServiceCommunicator;
         }
 
         /// <summary>
@@ -59,5 +62,20 @@ namespace GraphqlDemo.Operations
             return orderFaker.Generate(10);
             return null;
         }
+        public async Task<IEnumerable<CreateReviewDTO>> GetReviewsByDeliveryUserId(int deliveryUserId)
+        {
+            return await _reviewServiceCommunicator.GetReviewsByDeliveryUserId(deliveryUserId);
+        }
+        public async Task<IEnumerable<CreateReviewDTO>> GetReviewsByRestaurantId(int restaurantId)
+        {
+            return await _reviewServiceCommunicator.GetReviewsByRestaurantId(restaurantId);
+        }
+
+        public async Task<IEnumerable<CreateReviewDTO>> GetReviewsByUserId(int userId)
+        {
+            return await _reviewServiceCommunicator.GetReviewsByUserId(userId);
+        }
+
+
     }
 }
