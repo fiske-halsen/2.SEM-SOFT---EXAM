71bae3142501903abb7e218f017551971f607a5b
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2022-12-10 15:28:04 +0100

    unittest virker

--- Application/OrderService.Test/UnitTest/ServiceUnitTest.cs
+++ Application/OrderService.Test/UnitTest/ServiceUnitTest.cs
@@ -8,7 +8,7 @@ using OrderService.Services;
 
 namespace OrderService.Test.UnitTest
 {
-    public class Tests
+    public class ServiceUnitTest
     {
         private Mock<IOrderRepository> _orderRepositoryMock;
         private IOrderService _orderService;
@@ -28,9 +28,10 @@ namespace OrderService.Test.UnitTest
             {
                 CardType = CardTypes.Visa,
                 CustomerEmail = "test@test.dk",
-                MenuItems = new List<MenuItemDTO>
+                MenuItems = new List<CreateOrderMenuItemDto>
                 {
-                    new MenuItemDTO { Id = 1, description = "food", name = "pizza", price = 10.10, StockCount = 10 }
+                    new CreateOrderMenuItemDto { Id = 1, Price = 20 },
+                    new CreateOrderMenuItemDto { Id = 1, Price = 23 }
                 },
                 OrderTotal = 10,
                 PaymentType = PaymentTypes.CreditCard,
@@ -56,36 +57,21 @@ namespace OrderService.Test.UnitTest
         }
 
         [Test]
-        public async Task CancelOrderTest()
+        public async Task DeleteOrderTest()
         {
             //Arrange
             var orderId = 1;
 
             var dummyOrder = new Order { };
-            _orderRepositoryMock.Setup(_ => _.CancelOrder(orderId)).ReturnsAsync(dummyOrder);
+            _orderRepositoryMock.Setup(_ => _.DeleteOrder(dummyOrder)).ReturnsAsync(true);
+            _orderRepositoryMock.Setup(_ => _.GetOrderById(orderId)).ReturnsAsync(dummyOrder);
 
             //Act
-            var actualMocked = await _orderService.CancelOrder(orderId);
+            var actualMocked = await _orderService.DeleteOrder(orderId);
 
             //Assert
-            _orderRepositoryMock.Verify(mock => mock.CancelOrder(1), Times.Exactly(1));
+            _orderRepositoryMock.Verify(mock => mock.DeleteOrder(dummyOrder), Times.Exactly(1));
         }
-
-        //[Test]
-        //public async Task TimeToDeliveryTest()
-        //{
-        //    //Arrange
-        //    var orderId = 1;
-
-        //    _orderRepositoryMock.Setup(_ => _.TimeToDelivery(orderId)).ReturnsAsync(1030);
-
-        //    //Act
-        //    var actualMocked = await _orderService.TimeToDelivery(orderId);
-
-        //    //Assert
-        //    actualMocked.Should().Be(1030);
-        //    _orderRepositoryMock.Verify(mock => mock.TimeToDelivery(1), Times.Exactly(1));
-        //}
 
         [Test]
         public async Task AcceptOrderTest()
@@ -94,31 +80,147 @@ namespace OrderService.Test.UnitTest
             var orderId = 1;
 
             var dummyOrder = new Order { };
-            _orderRepositoryMock.Setup(_ => _.AcceptOrder(orderId)).ReturnsAsync(dummyOrder);
+            _orderRepositoryMock.Setup(_ => _.AcceptOrder(orderId)).ReturnsAsync(true);
+            _orderRepositoryMock.Setup(_ => _.GetOrderById(orderId)).ReturnsAsync(dummyOrder);
 
             //Act
             var actualMocked = await _orderService.AcceptOrder(orderId);
 
             //Assert
-            actualMocked.Should().Be(dummyOrder);
+            actualMocked.Should().Be(true);
             _orderRepositoryMock.Verify(mock => mock.AcceptOrder(1), Times.Exactly(1));
         }
 
         [Test]
-        public async Task DenyOrderTest()
+        public async Task GetAllOrdersForRestaurantTest()
+        {
+            //Arrange
+            var restaurantId = 1;
+            var isApproved = true;
+            var orders = new List<Order>();
+
+            var order1 = new Order
+            {
+                Id = 1,
+                CreatedAt = DateTime.Now,
+                IsActive = true,
+                IsApproved = false,
+                TotalPrice = 40,
+                CardType = CardTypes.Visa,
+                CustomerEmail = "test@test.dk",
+                PaymentType = PaymentTypes.CreditCard,
+                RestaurantId = 1,
+                MenuItems = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Id = restaurantId,
+                        ItemPrice = 23,
+                        MenuItemId = 1,
+                        OrderId = 1
+                    }
+                },
+            };
+
+            var order2 = new Order
+            {
+                Id = 1,
+                CreatedAt = DateTime.Now,
+                IsActive = true,
+                IsApproved = false,
+                TotalPrice = 40,
+                CardType = CardTypes.Visa,
+                CustomerEmail = "test@test.dk",
+                PaymentType = PaymentTypes.CreditCard,
+                RestaurantId = 1,
+                MenuItems = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Id = restaurantId,
+                        ItemPrice = 23,
+                        MenuItemId = 1,
+                        OrderId = 1
+                    }
+                },
+            };
+
+            orders.Add(order1);
+            orders.Add(order2);
+
+
+            var dummyOrder = new Order { };
+            _orderRepositoryMock.Setup(_ => _.GetAllOrdersForRestaurant(isApproved, restaurantId)).ReturnsAsync(orders);
+            _orderRepositoryMock.Setup(_ => _.GetAllOrdersForRestaurant(restaurantId)).ReturnsAsync(orders);
+
+            //Act
+            var actualMocked1 = await _orderService.GetAllOrdersForRestaurant(isApproved, restaurantId);
+            var actualMocked2 = await _orderService.GetAllOrdersForRestaurant(restaurantId);
+
+            //Assert
+            actualMocked1.Should().BeOfType<List<Order>>();
+            actualMocked2.Should().BeOfType<List<Order>>();
+            _orderRepositoryMock.Verify(mock => mock.GetAllOrdersForRestaurant(true, 1), Times.Exactly(1));
+            _orderRepositoryMock.Verify(mock => mock.GetAllOrdersForRestaurant(1), Times.Exactly(1));
+        }
+
+        [Test]
+        public async Task GetAllOrdersForUserTest()
+        {
+            //Arrange
+            var userEmail = "test@test.dk";
+
+            var order1 = new List<Order>
+            {
+                new Order
+                {
+                Id = 1,
+                CreatedAt = DateTime.Now,
+                IsActive = true,
+                IsApproved = false,
+                TotalPrice = 40,
+                CardType = CardTypes.Visa,
+                CustomerEmail = "test@test.dk",
+                PaymentType = PaymentTypes.CreditCard,
+                RestaurantId = 1,
+                MenuItems = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Id = 1,
+                        ItemPrice = 23,
+                        MenuItemId = 1,
+                        OrderId = 1
+                    }
+                }
+                }
+            };
+
+            _orderRepositoryMock.Setup(_ => _.GetOrdersForUser(userEmail)).ReturnsAsync(order1);
+
+            //Act
+            var actualMocked = await _orderService.GetAllOrdersForUser(userEmail);
+
+            //Assert
+            actualMocked.Should().BeOfType<List<Order>>();
+            _orderRepositoryMock.Verify(mock => mock.GetOrdersForUser(userEmail), Times.Exactly(1));
+        }
+
+        [Test]
+        public async Task UpdateOrderSetInActiveTest()
         {
             //Arrange
             var orderId = 1;
 
             var dummyOrder = new Order { };
-            _orderRepositoryMock.Setup(_ => _.DenyOrder(orderId)).ReturnsAsync(dummyOrder);
+            _orderRepositoryMock.Setup(_ => _.UpdateOrderSetInActive(dummyOrder)).ReturnsAsync(true);
+            _orderRepositoryMock.Setup(_ => _.GetOrderById(orderId)).ReturnsAsync(dummyOrder);
 
             //Act
-            var actualMocked = await _orderService.DenyOrder(1);
+            var actualMocked = await _orderService.UpdateOrderSetInActive(orderId);
 
             //Assert
-            actualMocked.Should().Be(dummyOrder);
-            _orderRepositoryMock.Verify(mock => mock.DenyOrder(1), Times.Exactly(1));
+            actualMocked.Should().BeTrue();
         }
     }
 }
